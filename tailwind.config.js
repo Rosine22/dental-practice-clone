c42b3945013268cee0d1734f7575e0501fe29344
Rosine22 <r.uwineza@alustudent.com>
2025-02-10 12:40:47 +0200

    updates

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+   content: ['./src/**/*.{html,js}'], 
+   theme: { 
+    extend: 
+    {
+      colors:{
+        Bigfonts :SofiaPro-Bold - 700 ,
+        smallfonts: SofiaPro-Regular - 400,
+        smaller:sans-serif,
+      }
+    } 
+  }, 
+   plugins: [] 
+  };
